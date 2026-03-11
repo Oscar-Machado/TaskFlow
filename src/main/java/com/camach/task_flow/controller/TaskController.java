000623319c57bbf2310a296804326fdbc32d8090
Oscar Machado <oscarmdas@hotmail.com>
2026-03-11 17:34:00 -0300

    Desenvolvimento do Controller

--- src/main/java/com/camach/task_flow/controller/TaskController.java
+++ src/main/java/com/camach/task_flow/controller/TaskController.java
@@ -1,4 +1,76 @@
 package com.camach.task_flow.controller;
 
+import com.camach.task_flow.dto.task.TaskRequestDTO;
+import com.camach.task_flow.dto.task.TaskResponseDTO;
+import com.camach.task_flow.infrastructure.entity.Task;
+import com.camach.task_flow.mapper.TaskMapper;
+import com.camach.task_flow.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/tasks")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final TaskService taskService;
+
+    // Criar task
+    @PostMapping
+    public TaskResponseDTO createTask(@RequestBody TaskRequestDTO dto){
+
+        Task task = TaskMapper.toEntity(dto);
+
+        Task savedTask = taskService.createTask(task);
+
+        return TaskMapper.toDTO(savedTask);
+
+    }
+
+    // Listar todas tasks
+    @GetMapping
+    public List<TaskResponseDTO> getAllTasks(){
+
+        return taskService.getAllTasks()
+                .stream()
+                .map(TaskMapper::toDTO)
+                .collect(Collectors.toList());
+
+    }
+
+    // Buscar tasks por usuário
+    @GetMapping("/user/{userId}")
+    public List<TaskResponseDTO> getTasksByUser(@PathVariable Long userId){
+
+        return taskService.getTasksByUserId(userId)
+                .stream()
+                .map(TaskMapper::toDTO)
+                .collect(Collectors.toList());
+
+    }
+
+    // Atualizar task
+    @PutMapping("/{id}")
+    public TaskResponseDTO updateTask(@PathVariable Long id,
+                                      @RequestBody TaskRequestDTO dto){
+
+        Task task = TaskMapper.toEntity(dto);
+        task.setId(id);
+
+        Task updatedTask = taskService.updateTask(task);
+
+        return TaskMapper.toDTO(updatedTask);
+
+    }
+
+    // Deletar task
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id){
+        taskService.deleteTask(id);
+
+    }
+
 }
--- src/main/java/com/camach/task_flow/controller/UserController.java
+++ src/main/java/com/camach/task_flow/controller/UserController.java
@@ -1,4 +1,75 @@
 package com.camach.task_flow.controller;
 
+import com.camach.task_flow.dto.user.UserRequestDTO;
+import com.camach.task_flow.dto.user.UserResponseDTO;
+import com.camach.task_flow.infrastructure.entity.User;
+import com.camach.task_flow.mapper.UserMapper;
+import com.camach.task_flow.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    //Criar User
+    @PostMapping
+    public UserResponseDTO creatUser (@RequestBody @Valid UserRequestDTO dto){
+
+        User user = UserMapper.toEntity(dto);
+
+        User savedUser = userService.createUser(user);
+
+        return UserMapper.toDTO(savedUser);
+
+    }
+
+    //Listar User
+    @GetMapping
+    public List<UserResponseDTO> getAllUsers(){
+
+        return userService.getAllUsers()
+                .stream()
+                .map(UserMapper::toDTO)
+                .collect(Collectors.toList());
+
+    }
+
+    //Buscar ByID
+    @GetMapping("/{id}")
+    public UserResponseDTO getUserById (@PathVariable Long id){
+
+        User user = userService.getUserById(id)
+                .orElseThrow(() -> new RuntimeException("User Not Found"));
+
+        return UserMapper.toDTO(user);
+
+    }
+
+    //Atualizar User
+    @PutMapping("/{id}")
+    public UserResponseDTO updateUser (@PathVariable Long id,
+                                       @RequestBody UserRequestDTO dto){
+        User user = UserMapper.toEntity(dto);
+        user.setId(id);
+
+        User updatedUser = userService.updateUser(user);
+
+        return UserMapper.toDTO(updatedUser);
+
+    }
+
+    // Deletar usuário
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+    }
+
 }
